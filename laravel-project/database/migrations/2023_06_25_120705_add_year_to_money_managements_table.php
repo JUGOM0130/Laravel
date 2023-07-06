3d83473c8315d55d5c8bdb1241308b871fb2d5c9
JUGOM0130 <junnehoomaki@yahoo.co.jp>
2023-07-06 09:14:39 +0900

    car_infoにDB追加

--- laravel-project/database/migrations/2023_06_25_120705_add_year_to_money_managements_table.php
+++ laravel-project/database/migrations/2023_06_25_120705_add_year_to_money_managements_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\MoneyManagements;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,6 +12,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if(Schema::hasColumn('money_managements','year')){
+            //money_managementsテーブルにyearカラムがあれば            
+            return;
+        }
         Schema::table('money_managements', function (Blueprint $table) {
             $table->integer('year')->after("id");
         });
--- laravel-project/database/migrations/2023_07_04_105010_create_car_infos_table.php
+++ laravel-project/database/migrations/2023_07_04_105010_create_car_infos_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('car_infos', function (Blueprint $table) {
             $table->id();
+            $table->string('car_name')->comment("車名");
+            $table->double('mode_urban',4,2)->comment("市街地");#zzzz.zz
+            $table->double('mode_suburbs',4,2)->comment("郊外");
+            $table->double('mode_highway',4,2)->comment("高速道路");
+            $table->double('mode_wltc',4,2)->comment("#WLTCモード");
+            $table->integer('price')->comment("価格");
+            $table->integer('model_year')->comment("年式");
+            $table->integer('mileage')->comment("走行距離");
+            $table->text('comment')->comment("コメント");
             $table->timestamps();
         });
     }
