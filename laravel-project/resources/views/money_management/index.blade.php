21ef1258897bbb45a32121f170eceedc7dbab3e6
JUGOM0130 <junnehoomaki@yahoo.co.jp>
2023-07-12 11:31:10 +0900

    bootstrap適応

--- laravel-project/resources/views/money_management/index.blade.php
+++ laravel-project/resources/views/money_management/index.blade.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- iconCDN -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <style>
         a.icon {
@@ -18,47 +23,49 @@
 </head>
 
 <body>
-    <h1>Link</h1>
-    <a href="{{route('money.toroku')}}">Create</a>
-    <div>
+    <div class="container">
+        <h1>一覧</h1>
+        <a href="{{ route('money.toroku') }}">Create</a>
+        <div>
 
-        <table>
-            <tr>
-                <th>詳細</th>
-                <th>年</th>
-                <th>月</th>
-                <th>カテゴリNo</th>
-                <th>カテゴリ名</th>
-                <th>金額</th>
-                <th>コメント</th>
-            </tr>
-            @foreach($list as $data)
-            <tr>
-                <td>
-                    <span class="material-symbols-outlined">
-                        <a class="icon" href={{route('money.show',['id'=>$data->id])}}>
-                            edit_note
-                        </a>
-                    </span>
-                </td>
-                <td>{{$data->year}}</td>
-                <td>{{$data->month}}</td>
-                <td>{{$data->category}}</td>
-                @if($data->category == '1')
-                <td>電気</td>
-                @endif
-                @if($data->category == '2')
-                <td>ガス</td>
-                @endif
-                @if($data->category == '3')
-                <td>水道</td>
-                @endif
-                <td>{{$data->kingaku}}</td>
-                <td>{{$data->comment}}</td>
+            <table class="table">
+                <tr>
+                    <th scope="col">詳細</th>
+                    <th scope="col">年</th>
+                    <th scope="col">月</th>
+                    <th scope="col">カテゴリNo</th>
+                    <th scope="col">カテゴリ名</th>
+                    <th scope="col">金額</th>
+                    <th scope="col">コメント</th>
+                </tr>
+                @foreach ($list as $data)
+                    <tr>
+                        <td>
+                            <span class="material-symbols-outlined">
+                                <a class="icon" href={{ route('money.show', ['id' => $data->id]) }}>
+                                    edit_note
+                                </a>
+                            </span>
+                        </td>
+                        <td>{{ $data->year }}</td>
+                        <td>{{ $data->month }}</td>
+                        <td>{{ $data->category }}</td>
+                        @if ($data->category == '1')
+                            <td>電気</td>
+                        @endif
+                        @if ($data->category == '2')
+                            <td>ガス</td>
+                        @endif
+                        @if ($data->category == '3')
+                            <td>水道</td>
+                        @endif
+                        <td>{{ $data->kingaku }}</td>
+                        <td>{{ $data->comment }}</td>
 
-            </tr>
-            @endforeach
-        </table>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     </div>
 </body>
 
