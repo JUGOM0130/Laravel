76f1b21562f289b2f07533bf462dad4c5c982339
JUGOM0130 <junnehoomaki@yahoo.co.jp>
2023-07-13 18:57:07 +0900

    vue環境構築開始

--- laravel-project/resources/js/app.js
+++ laravel-project/resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+/**20230713追加 */
+import { createApp, ref } from 'vue/dist/vue.esm-bundler';
+
+createApp({
+    setup() {
+
+        const teststring = ref('おはよ！');
+
+        return {
+            teststring,
+        }
+
+    },
+}).mount('#mamp');
+/**ここまで */
--- laravel-project/vite.config.js
+++ laravel-project/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+/**20230713追記 vueの本体？ */
+import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        /**20230713_追記 */
+        vue(),
     ],
+    /**20230713_追記 */
+    //ホットリロードが効かないときに使用すると良い
+    server:{
+        host:true
+    }
 });
