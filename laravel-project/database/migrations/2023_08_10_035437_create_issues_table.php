c219e3283cd219d8fc18ba0c1f71acf2a27ad5a9
JUGOM0130 <junnehoomaki@yahoo.co.jp>
2023-08-10 13:11:03 +0900

    add issue

--- laravel-project/database/migrations/2023_08_10_035437_create_issues_table.php
+++ laravel-project/database/migrations/2023_08_10_035437_create_issues_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('issues', function (Blueprint $table) {
             $table->id();
+            $table->string("company_cd")->comment("会社コード");
+            $table->string("category")->comment("カテゴリ\nバグ、要望、機能追加、質問");
+            $table->string("title")->comment("タイトル");
+            $table->string("detail")->comment("詳細");
+            $table->integer("priority")->comment("優先順位");
+            $table->string("task_flag")->comment("タスクフラグ\n対応完了、対応中、未着手、中断");
             $table->timestamps();
         });
     }
